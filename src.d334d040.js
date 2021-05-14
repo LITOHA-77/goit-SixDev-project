931bc84883997d260970d179f2ad3df4308a22d5
LITOHA-77 <77207925+LITOHA-77@users.noreply.github.com>
2021-05-14 07:59:14 +0000

    Deploying to gh-pages from @ LITOHA-77/goit-SixDev-project@333a1477f2f9d94101396e64886e081bac9768b5 🚀

--- src.d334d040.js
+++ src.d334d040.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-SixDev-project/src.060398e4.js.map
+//# sourceMappingURL=/goit-SixDev-project/src.d334d040.js.map
