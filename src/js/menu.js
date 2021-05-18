61d1208978c9905ef563648e8cc109a09f6899c5
ILLIA Kahanov <kahanov.illia@gmail.com>
2021-05-19 00:45:15 +0300

    features/update menu-btn
    add spin to burger-btn

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,32 @@
+// (() => {
+//     const refs = {
+//       openMenuBtn: document.querySelector('[data-menu-open]'),
+//       closeMenuBtn: document.querySelector('[data-menu-close]'),
+//       menu: document.querySelector('[data-menu]'),
+//     };
+  
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  
+//   function toggleMenu() {
+//       document.body.classList.toggle("scroll-lock");
+//       refs.menu.classList.toggle('is-close');
+//     }
+//   })();
+
+
 (() => {
     const refs = {
-      openMenuBtn: document.querySelector('[data-menu-open]'),
-      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      
+      MenuBtn: document.querySelector('[data-menu-btn]'),
       menu: document.querySelector('[data-menu]'),
     };
   
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.MenuBtn.addEventListener('click', toggleMenu);
   
   function toggleMenu() {
       document.body.classList.toggle("scroll-lock");
-      refs.menu.classList.toggle('is-close');
+    refs.menu.classList.toggle('is-close');
+    this.classList.toggle("is-active");
     }
   })();
